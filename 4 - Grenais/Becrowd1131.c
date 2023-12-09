84976d0d1ca0ab968262222e41e2d2bf19f219b1
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2023-12-09 18:53:22 -0300

    3 e 4

---
+++ 4 - Grenais/Becrowd1131.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int Inter,Gremio,VitInter,VitGremio,Empates,NumJogos,Op;
+    Inter=Gremio=VitInter=VitGremio=Empates=NumJogos=Op=0;
+
+    while(1)
+    {
+        scanf ("%d %d", &Inter, &Gremio);
+        if(Inter>Gremio) VitInter++;
+        if(Inter<Gremio) VitGremio++;
+        if(Inter==Gremio) Empates++;
+        NumJogos++;
+
+        printf("Novo grenal (1-sim 2-nao)\n");
+        scanf("%d", &Op);
+
+        if(Op==1){
+            continue;
+        }
+         if(Op==2){
+             break;
+         }   
+        
+        }
+        printf("%d grenais\n", NumJogos);
+        printf("Inter:%d\n", VitInter);
+        printf("Gremio:%d\n", VitGremio);
+        printf("Empates:%d\n", Empates);
+        if(VitInter>VitGremio){
+            printf("Inter venceu mais\n");
+        }
+        if(VitInter<VitGremio){
+            printf("Gremio venceu mais\n");
+        }
+        if(VitInter==VitGremio){
+            printf("Nao houve vencedor\n");
+        }
+        return 0;
+}
